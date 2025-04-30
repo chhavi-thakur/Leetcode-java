f344dad42e6928f3c84458ca509eff98a64f45cc
Chhavi thakur <thakurchhavi56@gmail.com>
2025-04-30 10:05:16 +0530

    Create SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,46 @@
+ public class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.') {
+                    // Try digits 1 to 9
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, row, col, c)) {
+                            board[row][col] = c; // Place the digit
+
+                            if (solve(board)) {
+                                return true; // If the board is solved
+                            } else {
+                                board[row][col] = '.'; // Backtrack
+                            }
+                        }
+                    }
+                    return false; // No valid digit found
+                }
+            }
+        }
+        return true; // Board completely filled
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[row][i] == c) return false;
+
+            // Check column
+            if (board[i][col] == c) return false;
+
+            // Check 3x3 sub-box
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == c) return false;
+        }
+        return true;
+    }
+
+}
