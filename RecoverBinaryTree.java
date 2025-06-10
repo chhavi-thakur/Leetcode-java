376971a9fd24847754aa272134e364a155ec5851
Chhavi thakur <thakurchhavi56@gmail.com>
2025-06-10 12:22:57 +0530

    Create RecoverBinaryTree.java

---
+++ RecoverBinaryTree.java
@@ -0,0 +1,30 @@
+ class Solution {
+    private TreeNode first = null;
+    private TreeNode second = null;
+    private TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        inorderTraversal(root);
+
+         int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorderTraversal(TreeNode root) {
+        if (root == null) return;
+
+         inorderTraversal(root.left);
+
+         if (first == null && prev.val > root.val) {
+            first = prev;
+        }
+        if (first != null && prev.val > root.val) {
+            second = root;
+        }
+
+         prev = root;
+
+         inorderTraversal(root.right);
+    }
+}
