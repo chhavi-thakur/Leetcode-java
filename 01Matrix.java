ecf8451c945349d96ae470f3316fa19aea2b5961
Chhavi thakur <thakurchhavi56@gmail.com>
2025-08-11 19:34:00 +0530

    Create 01Matrix.java

---
+++ 01Matrix.java
@@ -0,0 +1,42 @@
+ 
+public class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] dist = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+
+         for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+         int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+         while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int row = cell[0], col = cell[1];
+
+            for (int[] dir : dirs) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && !visited[newRow][newCol]) {
+                    dist[newRow][newCol] = dist[row][col] + 1;
+                    visited[newRow][newCol] = true;
+                    queue.offer(new int[]{newRow, newCol});
+                }
+            }
+        }
+
+        return dist;
+    }
+
+     
+
+}
